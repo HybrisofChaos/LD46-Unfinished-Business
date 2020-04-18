1a4a1d1f9233bbd1462298b2f1c190a5ed8b2f4f
Thomas Oosterbeek <thomasoosterbeek@hotmail.com>
2020-04-19 01:40:56 +0200

    Chain Fix, working on player movement

--- Unfinished Business/Scripts/Chain.gd
+++ Unfinished Business/Scripts/Chain.gd
@@ -4,11 +4,22 @@ const LOOP = preload("res://Objects/Loop.tscn")
 const LINK = preload("res://Objects/Link.tscn")
 
 export (int) var loops = 1
+export (NodePath) var player
 export (NodePath) var target
 
+var distance
+var direction
+var rot
+
 func _ready():
-	var parent = $Anchor
+	var parent = get_node(player)
 	target = get_node(target)
+	position = parent.global_position
+
+	distance = (target.position - position).length()
+	direction = (target.position - position).normalized()
+	loops = int(distance / 2)
+	rot = get_angle_to(target.position)
 
 	for _i in range (loops):
 		var child = addLoop(parent)
@@ -16,16 +27,18 @@ func _ready():
 		parent = child
 	
 	if target != null:
-		var child = addLoop(target)
-		addLink(parent, child)
+		addLink(parent, target)
 
 func addLoop(parent):
 	var loop = LOOP.instance()
 	loop.position = parent.position
-	loop.position.y += 4
+	loop.position += direction * 2
+	loop.look_at(parent.position)
+	loop.rotation += PI / 2
 	add_child(loop)
-	return loop
 
+	return loop
+	
 func addLink(parent, child):
 	var pin = LINK.instance()
 	pin.node_a = parent.get_path()
--- Unfinished Business/Scripts/Player.gd
+++ Unfinished Business/Scripts/Player.gd
@@ -1,24 +1,51 @@
 extends KinematicBody2D
 
-export (float) var max_speed = 64.0
-export (float) var acceleration = 8
+export (float) var speed = 32.0
 export (float) var gravity = 16
 export (float) var jump_speed = 256
 
-var velocity = Vector2.ZERO
+export (NodePath) var ball_path
+var max_ball_range = 48.0
+var ball
 
-func _physics_process(_delta):
-	if Input.is_action_pressed("player_right"):
-		velocity.x = min(velocity.x + acceleration, max_speed)
+func _ready():
+	ball = get_node(ball_path)
+	max_ball_range = (ball.position - position).length() + 3
+
+func _physics_process(delta):
+	var velocity = Vector2.ZERO
+
+	var distance = ball.position - position
+	var dX = max_ball_range - abs(distance.x)
+	var dY = max_ball_range - abs(distance.y)
+
 	if Input.is_action_pressed("player_left"):
-		velocity.x = max(velocity.x - acceleration, -max_speed)
+		velocity.x = max(-speed, -dX / delta)
+	if Input.is_action_pressed("player_right"):
+		velocity.x = min(speed, dX / delta)
+	if Input.is_action_pressed("player_up"):
+		velocity.y = max(-speed, -dY / delta)
+	if Input.is_action_pressed("player_down"):
+		velocity.y = min(speed, dY / delta)
+
+	#if velocity.x * delta > dX:
+	#	velocity.x = dX / delta
+		
+	velocity = move_and_slide(velocity, Vector2(0, -1))
+					
+
+# func _physics_process(_delta):
+# 	if Input.is_action_pressed("player_right"):
+# 		velocity.x = min(velocity.x + acceleration, max_speed)
+# 	if Input.is_action_pressed("player_left"):
+# 		velocity.x = max(velocity.x - acceleration, -max_speed)
 	
-	if is_on_floor():
-		if Input.is_action_pressed("player_jump"):
-			velocity.y = -jump_speed
+# 	if is_on_floor():
+# 		if Input.is_action_pressed("player_jump"):
+# 			velocity.y = -jump_speed
 	
 	
-	velocity.y += gravity
-	velocity.y = min(velocity.y, 500)
-	velocity.x = lerp(velocity.x, 0, 0.25)
-	velocity = move_and_slide(velocity, Vector2(0,-1))
+# 	velocity.y += gravity
+# 	velocity.y = min(velocity.y, 500)
+# 	velocity.x = lerp(velocity.x, 0, 0.25)
+# 	velocity = move_and_slide(velocity, Vector2(0,-1))
