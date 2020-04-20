4052c2b4c32769ca8f20470973f7f1ed9fb1788e
MartenMeijboom <MartenMeijboom@users.noreply.github.com>
2020-04-20 02:29:22 +0200

    glow and particles for crate movement

--- Unfinished Business/Scripts/MoveableObject.gd
+++ Unfinished Business/Scripts/MoveableObject.gd
@@ -2,42 +2,35 @@ extends RigidBody2D
 
 export (float) var movementCooldown = 1.5
 export (NodePath) var myBody
+export (NodePath) var myGlow
 
-var dragging = false
-var readyToShoot = false
-var shootToPos: Vector2
-var shootFromPos: Vector2
-var shootTimeout = false
+var originalPos: Vector2
 
-var cooldownTimer = 0
+var selected = false
 
 func _ready():
 	myBody = get_node(myBody)
-	pass
+	myGlow = get_node(myGlow)
 
-func _process(delta):
+func _process(_delta):
 	update() 
-	updateCooldownTimer(delta)
 	checkForInput()
 
-func updateCooldownTimer(delta):	
-	if shootTimeout:
-		cooldownTimer = cooldownTimer - delta
-		if cooldownTimer <= 0:
-			shootTimeout = false
-			cooldownTimer = movementCooldown
+func _physics_process(_delta):
+	myGlow.visible = selected
+	if selected:
+		gravity_scale = 0
+	else:
+		gravity_scale = 1	
 
 func checkForInput():
 	if Input.is_action_just_pressed("telekenesis"):
-		shootFromPos = get_global_mouse_position()
-		if(dragging):
-			dragging = false
-			if(readyToShoot && !shootTimeout):
-				shoot()
-				readyToShoot = false
-		elif checkIfMeantForMe():
-			shootToPos = get_global_mouse_position()
-			dragging = true
+		if !selected:
+			if checkIfMeantForMe():
+				originalPos = get_global_mouse_position()
+				selected = true
+		else:
+			selected = false
 
 func checkIfMeantForMe():
 	var objects = get_world_2d().direct_space_state.intersect_point(get_global_mouse_position())
@@ -47,24 +40,5 @@ func checkIfMeantForMe():
 	return false	
 
 func _input(event):
-	if event is InputEventMouseMotion:
-		shootFromPos = get_global_mouse_position()
-		if(dragging):
-			readyToShoot = true	
-
-func shoot():
-	var to = Vector2((shootToPos - global_position) - (shootFromPos - global_position))
-
-	apply_central_impulse(Vector2(to.x * 100, to.y * 110))
-
-	shootTimeout = true
-
-func _draw():
-	if dragging && !shootTimeout:
-		var object = position - global_position
-		var cursor = shootFromPos - shootToPos - object
-
-		#FUCKED
-		#draw_circle(shootToPos, 1.0, Color(1,1,1))
-
-		draw_line(object, object - cursor, Color(1, 1, 1), 1)		
+	if event is InputEventMouseMotion && selected:
+		global_position += event.relative
