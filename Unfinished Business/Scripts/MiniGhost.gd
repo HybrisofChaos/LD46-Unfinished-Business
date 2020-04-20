78425a68167651d0494c4eab5d29fdf0122f0afb
MartenMeijboom <MartenMeijboom@users.noreply.github.com>
2020-04-20 17:46:41 +0200

    Revert "start with astart"
    This reverts commit bab12d61988959f65a02e0958cb7d3b599146464.

--- Unfinished Business/Scripts/MiniGhost.gd
+++ Unfinished Business/Scripts/MiniGhost.gd
@@ -3,10 +3,10 @@ extends KinematicBody2D
 export (float) var speed
 
 func _physics_process(delta):
-	var velocity = Vector2.RIGHT.rotated(rotation) * speed
+    var velocity = Vector2.RIGHT.rotated(rotation) * speed
 
-	var collision = move_and_collide(velocity * delta)
+    var collision = move_and_collide(velocity * delta)
 
-	if collision:
-		#TODO: Do damage, explosion, whatever
-		queue_free()
+    if collision:
+        #TODO: Do damage, explosion, whatever
+        queue_free()
