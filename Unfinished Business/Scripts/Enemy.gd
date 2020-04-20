28849501d2449fe2d83ff9d3c19cad4065b60652
MartenMeijboom <MartenMeijboom@users.noreply.github.com>
2020-04-21 01:19:13 +0200

    update

--- Unfinished Business/Scripts/Enemy.gd
+++ Unfinished Business/Scripts/Enemy.gd
@@ -6,14 +6,14 @@ var max_health = 100
 var current_health = 100
 
 func _ready():
-    current_health = max_health
+	current_health = max_health
 
 func apply_damage(damage):
-    current_health -= damage
-    $AudioHurt.playing = false
-    $AudioHurt.playing = true
-    if current_health <= 0:
-        die()
+	current_health -= damage
+	$AudioHurt.playing = false
+	$AudioHurt.playing = true
+	if current_health <= 0:
+		die()
 
 func die():
-    queue_free()
+	queue_free()
